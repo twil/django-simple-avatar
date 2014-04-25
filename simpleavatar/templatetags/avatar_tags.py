0e4cf35969ebecfb86f7b9711716a066683083fb
Dmitry Slepichev <d.slepichev@gmail.com>
2014-04-25 23:17:01 +0400

    Update avatar_tags.py

--- simpleavatar/templatetags/avatar_tags.py
+++ simpleavatar/templatetags/avatar_tags.py
@@ -1,5 +1,6 @@
 import urllib
-from hashcompat import md5_constructor
+from hashlib import md5
+
 
 from django import template
 from django.contrib.auth.models import User
@@ -23,7 +24,7 @@ def avatar_url(user, size=80):
             if AVATAR_GRAVATAR_DEFAULT:
                 params['d'] = AVATAR_GRAVATAR_DEFAULT
             return "http://www.gravatar.com/avatar/%s/?%s" % (
-                md5_constructor(user.email).hexdigest(),
+                md5(user.email).hexdigest(),
                 urllib.urlencode(params))
         else:
             return AVATAR_DEFAULT_URL
